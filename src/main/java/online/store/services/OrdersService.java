704699903e04b4bec5ac403805393b463d601640
bhavitjain <bhavitjain902@gmail.com>
2023-08-03 23:55:38 +0530

    Prevent Large Purchases using an External Configuration

--- src/main/java/online/store/services/OrdersService.java
+++ src/main/java/online/store/services/OrdersService.java
@@ -4,18 +4,35 @@ import online.store.model.Order;
 import online.store.repositories.OrderRepository;
 import online.store.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrdersService {
-
-    @Autowired
     private OrderRepository orderRepository;
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final long maxNumberOfItems;
 
-    public void placeOrder(Iterable<Order> order){
-       orderRepository.saveAll(order);
+    public OrdersService(OrderRepository orderRepository,
+                         @Value("${products.service.max-number-of-items:25}") long maxNumberOfItems) {
+        this.orderRepository = orderRepository;
+        this.maxNumberOfItems = maxNumberOfItems;
+    }
+
+    private void validateNumberOfItemsOrdered(Iterable<Order> orders) {
+        long totalNumberOfItems = 0;
+        for (Order order: orders)  {
+            totalNumberOfItems += order.getQuantity();
+        }
+        if (totalNumberOfItems > maxNumberOfItems) {
+            throw new IllegalStateException(String.format("Number of products %d exceeded the limit of %d",
+                    totalNumberOfItems, maxNumberOfItems));
+        }
+    }
+
+    public void placeOrder(Iterable<Order> orders) {
+        validateNumberOfItemsOrdered(orders);
+
+        orderRepository.saveAll(orders);
     }
 }
